d37bcb2d3d21a9e75b120fe66ac261098ee337f1
Lindos-Mahadi <lindosmahadi@gmail.com>
2022-09-10 19:32:43 +0600

    Landing Page Desing Done && Adapt Layout

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -7,11 +7,13 @@ import { SearchSuggestion } from "./SearchSuggestion/SearchSuggestion";
 
 export function LandingPage(){
     return(
-        <div>
-            <TopNav />
-            <img src={logo} className={styles.logo} alt='logo'/>
-            <SearchBar />
-            <SearchSuggestion />
+        <div className={styles.landing}>
+            <div className={styles['search-area']}>
+                <TopNav />
+                <img src={logo} className={styles.logo} alt='logo'/>
+                <SearchBar />
+                <SearchSuggestion />
+            </div>
         </div>
     )
 }
