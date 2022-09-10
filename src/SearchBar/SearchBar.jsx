6558170d4a2a064c04b44a71118b9745f79ad35e
Lindos-Mahadi <lindosmahadi@gmail.com>
2022-09-10 17:38:08 +0600

    Landing pages' search bar Design

---
+++ src/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from './SearchBar.module.css';
+
+export function SearchBar() {
+    return ( 
+        <div>
+            <div className="field has-addons">
+                <p className="control">
+                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="tacos, cheap dinner, Max's" />
+                </p>
+                <p className="control">
+                    <button className="button is-medium is-dark">Search</button>
+                </p>
+                <p className="control">
+                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="address, neighborhood, city, state or zip" />
+                </p>
+                <p className="control">
+                    <button className="button is-medium is-dark">Near</button>
+                </p>
+                <div className={`button is-medium ${styles['search-button']}`}>
+                    <span className="icon is-small "><i className="fa fa-search"></i></span>
+                </div>
+            </div>
+        </div>
+     ); 
+}
