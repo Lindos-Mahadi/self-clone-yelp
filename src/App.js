3a24046f376ef9bc4ffb4a2362c6c9041565b2b2
Lindos-Mahadi <lindosmahadi@gmail.com>
2022-09-11 09:58:21 +0600

    Navigation bar Ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {Switch, Route} from "react-router-dom";
 import { LandingPage } from "./LandingPage/LandingPage";
+import {Search} from './Search/Search';
 
 function App() {
   return (
-    <LandingPage />
+    // <LandingPage />
+    <Switch>
+      <Route path='/search' component={Search}/>
+      <Route path='/' component={LandingPage}/>
+    </Switch>
   );
 }
 
--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import styles from './SearchBar.module.css';
 
-export function SearchBar() {
+export function SearchBar(props) {
+    const sizeClass = props.small ? '' : 'is-medium';
     return ( 
         <div>
             <div className="field has-addons">
                 <p className="control">
-                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="tacos, cheap dinner, Max's" />
+                    <input className={`input  ${sizeClass} ${styles['input-control']}`} type="text" placeholder="tacos, cheap dinner, Max's" />
                 </p>
                 <p className="control">
-                    <button className="button is-medium is-dark">Search</button>
+                    <button className={`button is-dark  ${sizeClass}`}>Search</button>
                 </p>
                 <p className="control">
-                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="address, neighborhood, city, state or zip" />
+                    <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="address, neighborhood, city, state or zip" />
                 </p>
                 <p className="control">
-                    <button className="button is-medium is-dark">Near</button>
+                    <button className={`button is-dark  ${sizeClass}`}>Near</button>
                 </p>
-                <div className={`button is-medium ${styles['search-button']}`}>
+                <div className={`button ${sizeClass}  ${styles['search-button']}`}>
                     <span className={`icon is-small ${styles['search-icon']}`}><i className="fa fa-search"></i></span>
                 </div>
             </div>
