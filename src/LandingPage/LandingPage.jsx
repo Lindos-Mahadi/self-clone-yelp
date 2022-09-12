c09e535590d0e571c7f445eb9e75a56bcaf1b3f8
Lindos-Mahadi <lindosmahadi@gmail.com>
2022-09-12 19:50:11 +0600

    landing page and search results list in URL

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -4,14 +4,34 @@ import logo from '../assets/logo/logo.png';
 import styles from './LandingPage.module.css'
 import { SearchBar } from "../SearchBar/SearchBar";
 import { SearchSuggestion } from "./SearchSuggestion/SearchSuggestion";
+// import useReactRouter from 'use-react-router';
+import useReactRouter from 'use-react-router';
+import { useHistory, useLocation } from "react-router-dom";
 
 export function LandingPage(){
+    
+    // const {history} = useReactRouter();
+    let {history} = useHistory();
+
+    // function search(term, location) {
+    //     const urlEncodedTerm = encodeURI(term);
+    //     const urlEncodedLocation = encodeURI(location);
+    //     history.push(`/search?find_desc=${urlEncodedTerm}&find_loc=${urlEncodedLocation}`);
+    // }
+
+
+    function search(term, location){
+        // const urlEncodedTerm = encodeURI(term);
+        // const urlEncodedLocation = encodeURI(location);
+        // history.push(`/search?find_desc=${urlEncodedTerm}&find_loc=${urlEncodedLocation}`);
+    }
+
     return(
         <div className={styles.landing}>
             <div className={styles['search-area']}>
                 <TopNav />
                 <img src={logo} className={styles.logo} alt='logo'/>
-                <SearchBar />
+                <SearchBar search={search}/>
                 <SearchSuggestion />
             </div>
         </div>
--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import styles from './SearchBar.module.css';
+import { useHistory, useLocation } from "react-router-dom";
 
 export function SearchBar(props) {
 
     const [term, setTerm] = useState(props.term || '');
     const [location, setLocation] = useState(props.location || '');
+    // Accessing the history instance created by React
+    const history = useHistory();
 
     function submit(e){
         console.log(term, location);
+        history.push(`/search?find_desc=${term}&find_loc=${location}`);
         e.preventDefault();
     }
  
@@ -21,9 +25,9 @@ export function SearchBar(props) {
                     type="text" 
                     placeholder="tacos, cheap dinner, Max's" />
                 </p>
-                <p className="control">
+                <div className="control">
                     <button className={`button is-dark  ${sizeClass}`}>Search</button>
-                </p>
+                </div>
                 <p className="control">
                     <input className={`input ${sizeClass} ${styles['input-control']}`} 
                     onChange={(e)=>setLocation(e.target.value)}
