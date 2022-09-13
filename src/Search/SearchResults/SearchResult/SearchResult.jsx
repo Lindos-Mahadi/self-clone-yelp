1c231b86896804fa7673d19062f0542b0bfcbe1a
Lindos-Mahadi <lindosmahadi@gmail.com>
2022-09-13 19:49:39 +0600

    Display tags and contact details of business

--- src/Search/SearchResults/SearchResult/SearchResult.jsx
+++ src/Search/SearchResults/SearchResult/SearchResult.jsx
@@ -10,6 +10,9 @@ export function SearchResult(props) {
         return (<div/>);
     }
 
+    const tags = b.categories.map(category => (<span className={`tag ${styles['business-tag']}`} key={b.id + category.title}>{category.title}</span>));
+    const addressLines = b.location.display_address.map(addressLine => <p key={b.id + addressLine}>{addressLine}</p>);
+
     return (
         <div className={styles['search-result']}>
             <img src={b.image_url} alt='business' className={styles['business-image']}/>
@@ -17,10 +20,14 @@ export function SearchResult(props) {
                 <h2 className="subtitle">{b.name}</h2>
                 <BusinessRating reviewCount={b.review_count} rating={b.rating} />
                 {/* <p>{b.price} {tags}</p> */}
+                <p>{b.price}  {tags}</p> 
             </div>
             <div className={styles['contact-info']}>
-                <p>{b.phone}</p>
+                {/* <p>{b.phone}</p> */}
                 {/* {addressLines} */}
+
+                <p>{b.phone}</p>
+                {addressLines}
             </div>
 
         </div>
