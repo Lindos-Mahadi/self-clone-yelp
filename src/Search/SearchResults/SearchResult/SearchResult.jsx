fc7a3fa784bc71c1ea7aef459cd94038c8515679
Lindos-Mahadi <lindosmahadi@gmail.com>
2022-09-13 19:23:22 +0600

    Display data from Yelp API in search results && Bug Fixed

--- src/Search/SearchResults/SearchResult/SearchResult.jsx
+++ src/Search/SearchResults/SearchResult/SearchResult.jsx
@@ -3,19 +3,24 @@ import React from 'react';
 import styles from './SearchResult.module.css';
 import { BusinessRating } from '../../../BusinessRating/BusinessRating';
 
-export function SearchResult() {
+export function SearchResult(props) {
+    console.log(props.business);
+    const b = props.business;
+    if (!b) {
+        return (<div/>);
+    }
+
     return (
         <div className={styles['search-result']}>
-            <img src='https://via.placeholder.com/210' alt='business' className={styles['business-image']}/>
+            <img src={b.image_url} alt='business' className={styles['business-image']}/>
             <div className={styles['business-info']}>
-                <h2 className="subtitle">Burger Place</h2>
-                <BusinessRating/>
-                <p>$$ <span className="tag">Burgers</span> <span className="tag">Fast Food</span></p>
+                <h2 className="subtitle">{b.name}</h2>
+                <BusinessRating reviewCount={b.review_count} rating={b.rating} />
+                {/* <p>{b.price} {tags}</p> */}
             </div>
             <div className={styles['contact-info']}>
-                <p>+49123456789</p>
-                <p>Example Street 5</p>
-                <p>12345 Berlin</p>
+                <p>{b.phone}</p>
+                {/* {addressLines} */}
             </div>
 
         </div>
